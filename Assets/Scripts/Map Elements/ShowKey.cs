b51ef0a93fab7f718678f24f83d3714f2c900710
chbouhin <christophe.bouhin@epitech.eu>
2022-11-22 12:19:50 +0100

    Show key on interactable obj

---
+++ Assets/Scripts/Map Elements/ShowKey.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ShowKey : MonoBehaviour
+{
+    [SerializeField] private TextMeshPro textKey;
+    [SerializeField] private Animator animator;
+    [SerializeField] private bool squareCanInteract;
+    private InputManager inputManager;
+
+    private void Start()
+    {
+        inputManager = FindObjectOfType<InputManager>();
+        if (squareCanInteract)
+            ChangeKey(inputManager.GetKeyCodes("SquareInteract"));
+        else
+            ChangeKey(inputManager.GetKeyCodes("MouseLightInteract"));
+    }
+
+    public void ChangeKey(KeyCodes keys)
+    {
+        textKey.text = "";
+        if (keys.key1 != KeyCode.None)
+            textKey.text += keys.key1.ToString();
+        if (keys.key2 != KeyCode.None) {
+            if (keys.key1 != KeyCode.None)
+                textKey.text += "|";
+            textKey.text += keys.key2.ToString();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (squareCanInteract) {
+            if (col.gameObject.tag == "Square")
+                animator.SetBool("Open", true);
+        } else {
+            if (col.gameObject.tag == "MouseLight")
+                animator.SetBool("Open", true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (squareCanInteract) {
+            if (col.gameObject.tag == "Square")
+                animator.SetBool("Open", false);
+        } else {
+            if (col.gameObject.tag == "MouseLight")
+                animator.SetBool("Open", false);
+        }
+    }
+}
